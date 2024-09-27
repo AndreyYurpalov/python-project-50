9b5632cc767e1b24939a43037a6eb6dc5e51349b
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-09-27 12:03:10 +0500

    modified scripts/getdiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,13 +1,19 @@
 import argparse
+from gendiff.gendiff import generate_diff
+from pathlib import Path
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.'
+    )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-
+    parser.add_argument('-f', '--format',
+                        help='set format of output')
     args = parser.parse_args()
+    result = generate_diff(Path(args.first_file), Path(args.second_file))
+    print(result)
 
 
 if __name__ == "__main__":
