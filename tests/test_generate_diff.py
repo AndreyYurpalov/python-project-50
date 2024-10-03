f1cc0d4d6512059acdf629f5ae58bfffe92c66bc
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-10-03 11:59:00 +0500

    modified test_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,10 +1,16 @@
 from gendiff.gendiff import generate_diff
 
 
-file1 = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file1.json"
-file2 = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file2.json"
+file1_json = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file1.json"
+file2_json = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file2.json"
+file1_yml = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file1.yml"
+file2_yml = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file2.yml"
+
 diff_files = "/home/ayu_263_ekb/python-project-50/tests/fixtures/diff_file1_file2"
 with open(diff_files) as f:
     diff = f.read()
 def test_generate_diff():
-    assert generate_diff(file1, file2) == diff
+    assert generate_diff(file1_json, file2_json) == diff
+    assert generate_diff(file1_yml, file2_yml) == diff
+    assert generate_diff(file1_json, file2_yml) == diff
+    assert generate_diff(file1_yml, file2_json) == diff
