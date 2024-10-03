d197069842698d9a10b6669045bcb9225c014ba8
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-10-03 16:20:16 +0500

    modified test_generate_diff.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,14 +1,20 @@
 from gendiff.gendiff import generate_diff
+import os
+
+def get_abspath(file):
+    file = str(file)
+    return os.path.abspath(f'tests/fixtures/{file}')
 
 
-file1_json = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file1.json"
-file2_json = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file2.json"
-file1_yml = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file1.yml"
-file2_yml = "/home/ayu_263_ekb/python-project-50/tests/fixtures/file2.yml"
-
-diff_files = "/home/ayu_263_ekb/python-project-50/tests/fixtures/diff_file1_file2"
+file1_json = get_abspath('file1.json')
+file2_json = get_abspath('file2.json')
+file1_yml = get_abspath('file1.yml')
+file2_yml = get_abspath('file2.yml')
+diff_files = get_abspath('diff_file1_file2')
 with open(diff_files) as f:
     diff = f.read()
+
+
 def test_generate_diff():
     assert generate_diff(file1_json, file2_json) == diff
     assert generate_diff(file1_yml, file2_yml) == diff
