cbba82605c829a96862a051a1681ecb0bf1d7dd5
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-10-25 13:36:17 +0500

    gendiff/gendiff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,7 +1,9 @@
-import itertools
 import json
 import yaml
 import os
+from gendiff.formatters.format_stylish import get_stylish
+from gendiff.formatters.format_plain import get_plain_tree
+from gendiff.formatters.format_json import get_format_json
 
 
 def get_file(file):
@@ -17,22 +19,14 @@ def get_dic_to_generate_diff(path_f):
 def get_dic(pathfile):
     _, ext = os.path.splitext(pathfile)
     if ext == '.yml' or ext == '.yaml':
-        fy = get_dic_to_generate_diff(pathfile)
-        dic = yaml.load(fy, Loader=yaml.FullLoader)
+        file_yaml = get_dic_to_generate_diff(pathfile)
+        dic = yaml.safe_load(file_yaml)
     elif ext == '.json':
-        fj = get_dic_to_generate_diff(pathfile)
-        dic = json.loads(fj)
+        file_json = get_dic_to_generate_diff(pathfile)
+        dic = json.loads(file_json)
     else:
         dic = 'ERROR'
     return dic
-
-
-def str_lower(word):
-    if isinstance(word, bool):
-        return str(word).lower()
-    if word is None:
-        return 'null'
-    return word
 
 
 def get_diff_tree(dic1, dic2):
@@ -62,58 +56,18 @@ def get_diff_tree(dic1, dic2):
     return inner(None, dic1, dic2)
 
 
-def get_stylish(diff, replase=' ', indent=4):
-    mark = {'added': '+ ', 'deleted': '- ', 'unchanged': '  ', 'nested': '  '}
-
-    def get_ch_diff(diff, level):
-        if isinstance(diff, dict):
-            line = []
-            mark_size = 2
-            indent_size = indent * level
-            currient_indent = (indent_size - mark_size) * replase
-            for key, val in diff.items():
-                line.append(f"{currient_indent}{mark['nested']}{key}: "
-                            f"{get_ch_diff(val, level + 1)}")
-            currient_indent = indent * (level - 1) * replase
-            result = itertools.chain('{', line, [currient_indent + '}'])
-            return '\n'.join(result)
-        return str_lower(diff)
-
-    def inner(diff, level):
-        line = []
-        mark_size = 2
-        indent_size = indent * level
-        line_indent = (indent_size - mark_size) * replase
-        for child in diff.get('childs'):
-            type = str(child['type'])
-            if type == 'nested':
-                line.append(f"{line_indent}{mark[type]}{child['name']}: "
-                            f"{inner(child, level + 1)}")
-            elif type == 'changed':
-                line.append(f"{line_indent}{mark['deleted']}{child['name']}: "
-                            f"{get_ch_diff(child['value']['old'], level + 1)}")
-
-                line.append(f"{line_indent}{mark['added']}{child['name']}: "
-                            f"{get_ch_diff(child['value']['new'], level + 1)}")
-            else:
-                line.append(f"{line_indent}{mark[type]}{child['name']}: "
-                            f"{get_ch_diff(child['value'], level + 1)}")
-
-        line_indent = indent * (level - 1) * replase
-        result = itertools.chain('{', line, [line_indent + '}'])
-        return '\n'.join(result)
-
-    return inner(diff, 1)
-
-
 def get_format(diff, format):
     if format == 'stylish':
         return get_stylish(diff)
+    if format == 'plain':
+        return get_plain_tree(diff)
+    if format == 'json':
+        return get_format_json(diff)
 
 
-def generate_diff(pathfile1, pathfil2, format_name='stylish'):
+def generate_diff(pathfile1, pathfile2, format_name='stylish'):
     dic1 = get_dic(pathfile1)
-    dic2 = get_dic(pathfil2)
+    dic2 = get_dic(pathfile2)
     diff = get_diff_tree(dic1, dic2)
     result = get_format(diff, format_name)
 
