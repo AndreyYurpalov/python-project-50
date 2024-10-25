04395cfa2816b6499cf72113a159d355e3f1cd2b
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-10-25 14:40:28 +0500

    modified test_generate.py

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -9,17 +9,20 @@ def get_abspath(file):
 
 
 date_files = [
-    ('file1.json', 'file2.json', 'diff_file1_file2'),
-    ('file1.json', 'file2.yml', 'diff_file1_file2'),
-    ('file1.yml', 'file2.json', 'diff_file1_file2'),
-    ('file1.yml', 'file2.yml', 'diff_file1_file2'),
-    ('file3.json', 'file4.json', 'diff_file3_file4'),
-    ('file3.json', 'file4.yml', 'diff_file3_file4'),
-    ('file3.yml', 'file4.json', 'diff_file3_file4'),
-    ('file3.yml', 'file4.yml', 'diff_file3_file4'),]
+    ('file1.json', 'file2.json', 'diff_file1_file2.txt'),
+    ('file1.json', 'file2.yml', 'diff_file1_file2.txt'),
+    ('file1.yml', 'file2.json', 'diff_file1_file2.txt'),
+    ('file1.yml', 'file2.yml', 'diff_file1_file2.txt'),
+    ('file3.json', 'file4.json', 'diff_file3_file4.txt'),
+    ('file3.json', 'file4.yml', 'diff_file3_file4.txt'),
+    ('file3.yml', 'file4.json', 'diff_file3_file4.txt'),
+    ('file3.yml', 'file4.yml', 'diff_file3_file4.txt'),]
 
 
 @pytest.mark.parametrize('file1, file2, result_file', date_files)
 def test_generate_diff(file1, file2, result_file):
     result = open(get_abspath(result_file)).read()
+    if ':\n' in result:
+        result = result.replace(':\n', ': \n')
+
     assert (generate_diff(get_abspath(file1), get_abspath(file2)) == result)
