76b6d0d35b1601a6f308ae55776a88fd7a758098
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-11-05 12:22:28 +0500

    modified gendiff/gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -8,11 +8,11 @@ from gendiff.formatters.format_diff import get_format
 def get_dict_to_generate_diff(path_f):
     _, ext = os.path.splitext(path_f)
     with open(path_f) as f:
-        dic = f.read()
-    return get_dict_file(dic, ext)
+        opened_file = f.read()
+    return parse(opened_file, ext)
 
 
-def get_dict_file(file, ext):
+def parse(file, ext):
     if ext == '.yml' or ext == '.yaml':
         dictionary = yaml.safe_load(file)
     elif ext == '.json':
@@ -22,7 +22,9 @@ def get_dict_file(file, ext):
 
 def generate_diff(path_file1, path_file2, format_name='stylish'):
     dict1 = get_dict_to_generate_diff(path_file1)
+    print(dict1)
     dict2 = get_dict_to_generate_diff(path_file2)
+    print(dict2)
     diff = get_diff_tree(dict1, dict2)
     result = get_format(diff, format_name)
     return result
