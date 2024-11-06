dfa9dc4cd972f1f3d5793aa1b01ab6113bbb757b
Andrey Yurpalov <a.yurpalov@mail.ru>
2024-11-06 15:39:39 +0500

    modified gendiff/formatters/format_plain.py

--- gendiff/formatters/format_plain.py
+++ gendiff/formatters/format_plain.py
@@ -1,4 +1,5 @@
 from gendiff.formatters.str_lower import str_lower
+from gendiff.tree_diff import ADDED, DELETED, UNCHANGED, CHANGED, NESTED
 
 
 def to_str(value):
@@ -13,25 +14,25 @@ def get_plain_tree(diff):
     def func(diff, path):
         line = []
         for child in diff.get('childs'):
-            if child['type'] == 'nested':
+            if child['type'] == NESTED:
                 res = func(child, path + child['name'] + '.')
                 text = f"{res}"
                 line.append(text)
-            elif child['type'] == 'added':
+            elif child['type'] == ADDED:
                 text = (f"Property \'{path + child['name']}\' "
                         f"was added with value: "
                         f"{to_str(child['value'])}")
                 line.append(text)
-            elif child['type'] == 'deleted':
+            elif child['type'] == DELETED:
                 text = f"Property \'{path + child['name']}\' was removed"
                 line.append(text)
-            elif child['type'] == 'changed':
+            elif child['type'] == CHANGED:
                 val = child['value']
                 text = (f"Property \'{path + child['name']}\' "
                         f"was updated. From {to_str(val['old'])} "
                         f"to {to_str(val['new'])}")
                 line.append((text))
-            elif child['type'] == 'unchanged':
+            elif child['type'] == UNCHANGED:
                 continue
         return '\n'.join(line)
     return func(diff, '')
